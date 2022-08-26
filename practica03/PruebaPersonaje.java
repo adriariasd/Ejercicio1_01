2a789f53234e710cde13e3d1c95dee21f59f7d93
adriariasd <adriariasd@gmail.com>
2022-08-26 10:21:34 -0500

    practica03 finalizado 10:21
    revisado

--- practica03/PruebaPersonaje.java
+++ practica03/PruebaPersonaje.java
@@ -1,11 +1,11 @@
 public class PruebaPersonaje {
     public static void main(String[] args) {
         Personaje alumno = new Personaje();
-        alumno.setNombre("KobeBryant");
+        alumno.setNombre("Kobe Bryant");
         alumno.saludar();
         System.out.println("Modificando el nombre" + alumno.getNombre());
         alumno.setNombre("Michael Jordan");
         alumno.saludar();
-        
+
     }
 }
