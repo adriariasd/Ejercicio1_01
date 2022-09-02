c38521e97f785ab97354dbc1fcc05a5c7cc98420
adriariasd <adriariasd@gmail.com>
2022-09-02 08:42:50 -0500

    practica04 8:42

--- practica04/PruebaPersonaje.java
+++ practica04/PruebaPersonaje.java
@@ -4,6 +4,16 @@ public class PruebaPersonaje {
         alumno.saludar();
         System.out.println("Nombre: "+alumno.getNombre()+"Edad: "+alumno.getEdad());
         System.out.println("Modificando el nombre "+alumno.getNombre());
-        alumno.setNombre
+        alumno.setNombre("Michael Jordan");
+        alumno.saludar();
+        if (!alumno.setEdad(30)){
+            System.out.println("Edad sin cambios");
+        }
+        System.out.println("Nombre: "+alumno.getNombre()+"Edad: "+alumno.getEdad());
+        System.out.println("Modificando edad en 130");
+        if(!alumno.setEdad(130)){
+            System.out.println("Edad sin cambio");
+        }
+        System.out.println("Nombre: "+alumno.getNombre()+"Edad: "+alumno.getEdad());
     }
 }
