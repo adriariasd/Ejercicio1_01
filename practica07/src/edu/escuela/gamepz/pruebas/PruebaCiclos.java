66c97c41cd70444851e3dfa85636146d0c7b6343
adriariasd <adriariasd@gmail.com>
2022-09-23 08:18:40 -0500

    practica07

--- practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
+++ practica07/src/edu/escuela/gamepz/pruebas/PruebaCiclos.java
@@ -1,11 +1,16 @@
 package edu.escuela.gamepz.pruebas;
 import edu.escuela.gamepz.personajes.*;
 public class PruebaCiclos {
-    Personaje per01 = new Personaje("Almendra");
-	Personaje per02 = new Personaje("Ricardo");
+    Personaje per01 = new Personaje("Diego");
+	Personaje per02 = new Personaje("Almendra");
 	Personaje per03 = new Personaje("Bianca");
-	Personaje per04 = new Personaje("Leonel");
+	Personaje per04 = new Personaje("Jordan");
     int n=0;
     int m=0;
-    while
+    while (n>=m){
+        n=(int)Math.random();
+        m=(int)Math.random();
+    }
+    System.out.println("n = "+n);
+    System.out.println("m = "+m);
 }
