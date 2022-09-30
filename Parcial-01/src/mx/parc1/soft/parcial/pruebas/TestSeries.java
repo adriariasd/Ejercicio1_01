5ca3199c4e1bdd145fc08943bdb720ab80b44129
adriariasd <adriariasd@gmail.com>
2022-09-30 08:41:38 -0500

    Parcial 1 08:41

--- Parcial-01/src/mx/parc1/soft/parcial/pruebas/TestSeries.java
+++ Parcial-01/src/mx/parc1/soft/parcial/pruebas/TestSeries.java
@@ -14,9 +14,9 @@ public class TestSeries {
 
         int n = s.genNumero(100);
 
-        s.fibo(n);
+        //s.fibo(n);
 
-        int p = s.fiboN(3);
+        int p = s.fiboN(n);
 
         System.out.println(p);
 
---
+++ Parcial-01/src/mx/parc1/soft/parcial/series/Series.java
@@ -0,0 +1,17 @@
+public class Series {
+    public void Serie(){
+
+    }
+    public int genNumero(int num){
+        int x;
+        if (num<10) {
+            x=100;
+            return x;
+        } else {
+            
+        }
+    }
+    public int fiboN(int num){
+
+    }
+}
