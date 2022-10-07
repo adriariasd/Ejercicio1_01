dafe2590eb599e6b20f225f923135ec98fda872f
adriariasd <adriariasd@gmail.com>
2022-10-07 08:53:42 -0500

    practica08 8:53

--- practica08/src/edu/escuela/gamepz/personajes/buenos/Planta.java
+++ practica08/src/edu/escuela/gamepz/personajes/buenos/Planta.java
@@ -4,6 +4,7 @@ public class Planta extends Personaje{
     char escudo;
     public Planta(String nombre, int vida, char escudo){
         super(nombre, vida);
+        this.escudo = escudo;
     }
     public Planta(String nombre, char escudo){
         this(nombre, 3, escudo);
@@ -14,6 +15,15 @@ public class Planta extends Personaje{
     public Planta(String nombre){
         this(nombre, 3, 'A');
     }
+    public void decVida(){
+		super.setVida(vida-1);
+	}
+	public void decVida(int dec){
+		setVida(vida-dec);
+	}
+    public String getDetalle(){
+		return super.getDetalle()+" "+escudo;
+	}
     public char getEscudo(){
         return escudo;
     }
