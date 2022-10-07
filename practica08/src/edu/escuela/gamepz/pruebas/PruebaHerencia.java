a730ac95bfeaf6fae9780b0eeb106d344b29c7e8
adriariasd <adriariasd@gmail.com>
2022-10-07 09:29:11 -0500

    practica08 9:29

--- practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
+++ practica08/src/edu/escuela/gamepz/pruebas/PruebaHerencia.java
@@ -1,7 +1,17 @@
 package edu.escuela.gamepz.pruebas;
-
+import edu.escuela.gamepz.personajes.*;
+import edu.escuela.gamepz.personajes.buenos.*;
+import edu.escuela.gamepz.personajes.malos.*;
 public class PruebaHerencia {
     public static void main(String[] args) {
-        
+        Personaje per01 = new Personaje("David", 100);
+        Personaje per02 = new Personaje("Bianca");
+        Planta plan01 = new Planta("Fabian", 10, 'B');
+        Planta plan02 = new Planta("Almendra", 50);
+        Planta plan03 = new Planta("Ricardo", 'C');
+        Planta plan04 = new Planta("Silvia");
+        Zombie zom01 = new Zombie("Armando", 80, false);
+        Zombie zom02 = new Zombie("Josseline", true);
+        Zombie zom03 = new Zombie("Eduardo");
     }
 }
