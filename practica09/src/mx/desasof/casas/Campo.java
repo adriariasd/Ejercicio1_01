db406b9dc9066404925ab83f088dfd0cfec08a20
adriariasd <adriariasd@gmail.com>
2022-10-14 08:26:45 -0500

    practica09 8:26

--- practica09/src/mx/desasof/casas/Campo.java
+++ practica09/src/mx/desasof/casas/Campo.java
@@ -1,9 +1,16 @@
 package mx.desasof.casas;
 
 public class Campo extends Casa{
-    float[2] piscina;
+    float[] piscina;
+    piscina = new float[2];
     public Casa(String tipo, int hab, float costo, float x, float y){
         super(tipo, hab, costo);
 
+    }
+    public int hashCode(){
+        return super.hashCode()*x*y/5;
+    }
+    public boolean equals(Object ob){
+        return super.equals() && ob instanceof Campo; 
     }
 }
