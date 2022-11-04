c03098c16ca7f21fad1d6c2483bfa3f6e300a2ad
adriariasd <adriariasd@gmail.com>
2022-11-04 08:59:15 -0600

    practica12 9:00

--- practica12/src/edu/escuela/gamepz/utils/Tablero.java
+++ practica12/src/edu/escuela/gamepz/utils/Tablero.java
@@ -1,5 +1,13 @@
 package edu.escuela.gamepz.utils;
-
+import edu.escuela.gamepz.personajes.Personaje;
 public class Tablero {
     public static final int MAX_SIZE = 10;
+    public Personaje[] personajes = new Personaje[MAX_SIZE];
+    private Tablero(){}
+    public void mostrar(){
+        System.out.println("\nInicio * * * Contenido del arreglo en Tablero");
+        for (Personaje p : personajes) {
+            
+        }
+    }
 }
