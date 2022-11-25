fa38cef513160f20ac8f0181b1d2216d97fb9bbf
adriariasd <adriariasd@gmail.com>
2022-11-25 09:24:03 -0600

    practica15 9:23
    desayuno

--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -7,8 +7,16 @@ public class RunHilos {
     }
     public static void iniciar(JTextField txt1, JTextField txt2, JLabel elem1, JLabel elem2){
         char ch1, ch2;
+        String salida1, salida2;
         if(txt1.getText().len != 0){
             ch1 = txt1.getText().charAt(0);
+        } else{
+            ch1 = 'E';
+        }
+        if(txt2.getText().len != 0){
+            ch2 = txt2.getText().charAt(0);
+        } else{
+            ch2 = 'M';
         }
     }
 }
