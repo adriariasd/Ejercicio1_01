154d76ef837ba509a98f0b7a6652c966e367bff1
adriariasd <adriariasd@gmail.com>
2022-11-25 09:59:26 -0600

    practica15 10:00

--- practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
+++ practica15/src/mx/com/hilos/corredores/hilos/RunHilos.java
@@ -8,15 +8,17 @@ public class RunHilos {
     public static void iniciar(JTextField txt1, JTextField txt2, JLabel elem1, JLabel elem2){
         char ch1, ch2;
         String salida1, salida2;
-        if(txt1.getText().len != 0){
+        if(txt1.getText().length() != 0){
             ch1 = txt1.getText().charAt(0);
         } else{
             ch1 = 'E';
         }
-        if(txt2.getText().len != 0){
+        if(txt2.getText().length() != 0){
             ch2 = txt2.getText().charAt(0);
         } else{
             ch2 = 'M';
         }
+        salida1 = String.valueOf(ch1);
+        salida2 = String.valueOf(ch2);
     }
 }
