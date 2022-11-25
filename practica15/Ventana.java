7a7eb82f352439e814fbb19c6389045bf66af501
adriariasd <adriariasd@gmail.com>
2022-11-25 08:42:46 -0600

    practica15 8:42

---
+++ practica15/Ventana.java
@@ -0,0 +1,33 @@
+package mx.com.hilos.ventanas.ventana;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+public class Ventana extends JFrame{
+    private JLabel lblTitulo, lblElem1, lblElem2, hElem1, hElem2;
+    private JTextField txtElem1, txtElem2;
+    private JButton btnStart;
+    public Ventana(){
+        initComponents();
+    }
+    public void initComponents(){
+        int ancho = 550;
+        int alto = 250;
+        Dimension lblDim = new Dimension(ancho-15,alto/10);
+        Color border = new Color(0, 0, 255);
+        lblTitulo = new JLabel("* Practica 15 Manejo de hilos *");
+        lblTitulo.setPreferredSize(lblDim);
+        lblElem1 = new JLabel("Hilo 1");
+        txtElem1 = new JTextField(3);
+        lblElem2 = new JLabel("Hilo 2");
+        txtElem2 = new JTextField(3);
+        btnStart = new JButton("Comenzar");
+        hElem1 = new JLabel(" Aqui va el hilo 1 ");
+        hElem1.setBorder(BorderFactory.createLineBorder(border));
+        hElem1.setPreferredSize(lblDim);
+        hElem2 = new JLabel(" Aqui va el hilo 2 ");
+        hElem2.setBorder(BorderFactory.createLineBorder(border));
+        hElem2.setPreferredSize(lblDim);
+        btnStart.setPreferredSize(lblDim);
+    }
+}
